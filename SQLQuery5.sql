fa07dc78bee85e37d71cf79570197ab033fe5538
tenzin <tenthinten@gmail.com>
2020-12-30 21:59:26 +0530

    Thinley's files

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -14,8 +14,10 @@ SELECT * from Territories
 
 
 create table example(column1 varchar(10),column2 int default 0)
-insert into example(column1) values('huuu')
+insert into example(column1) 
+values('huuu')
 select * from example
+drop table example
 
 SELECT distinct EmployeeID from EmployeeTerritories
 
